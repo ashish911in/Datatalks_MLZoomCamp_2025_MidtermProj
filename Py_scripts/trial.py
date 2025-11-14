0272b99fb1ee31018dd3f4a1179af6c3d0194ee3
Ashish Kumar <58314686+ashish911in@users.noreply.github.com>
2025-11-15 05:55:46 +1100

    sample docker file and sample trial py script

---
+++ Py_scripts/trial.py
@@ -0,0 +1,8 @@
+def sum_two_numbers(a, b):
+    return a + b
+
+# Example usage
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+result = sum_two_numbers(a, b)
+print(f'Result is {result}')
